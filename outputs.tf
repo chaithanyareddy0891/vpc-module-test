96003809b9f048f8fd61e07ec8bb2b08d13a5e14
chaithanyareddy <chaithanya.reddy0891@gmail.com>
2026-02-25 18:51:46 +0530

    vpc test module

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+# output "vpc_id" {
+#     value = module.vpc.vpc_id
+# }
+
+# output "igw_id" {
+#     value = module.vpc.igw_id
+# }
+
+# output "az_info" {
+#     value = module.vpc.az_info
+# }
+
+# output "default_vpc_info" {
+#     value = module.vpc.default_vpc_info
+# }
